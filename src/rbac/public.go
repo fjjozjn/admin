dc6430e78cff280bf28f7da1e9ce823e9d47851b
fjjozjn <fjjozjn@gmail.com>
2018-04-01 22:34:21 +0800

    还原远程调试代码

--- src/rbac/public.go
+++ src/rbac/public.go
@@ -70,21 +70,17 @@ func (this *MainController) Login() {
 			//更新最后登入时间
 			m.UpdateUser(&m.User{Id: user.Id, Lastlogintime: time.Now()})
 
-			log.Println(beego.AppConfig.String("invoice_system_host"))
 			//给invoice系统设置session，并记录session id
 			//http://aaltd-invoice.cxm/auth/auth.php?aAdminEmail=2322289219@qq.com&aFtyName=fjjozjn&aID=2&aLogin=fjjozjn&aName=fjjozjn&aNameChi=zjn
 			resp, err := http.Get(beego.AppConfig.String("invoice_system_host") + "auth/auth.php?aAdminEmail=" + user.Email + 
 				"&aFtyName=" + user.Username + "&aID=" + strconv.FormatInt(user.Id, 10) + "&aLogin=" + user.Username +
 				"&aName=" + user.Username + "&aNameChi=" + user.Nickname)
-			log.Println("1")
 			defer resp.Body.Close()
 			if err != nil {
-				log.Println("2")
 				log.Println(err)
 			}
     		body, err := ioutil.ReadAll(resp.Body)
     		if err != nil {
-    			log.Println("3")
     			log.Println(err)
     		}
  			this.SetSession("invoice_system_session_id", string(body))
