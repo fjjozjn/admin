082a9afa247ca8b786732d59a055735e05bdf204
Johnny Zhang <Johnny.z@lifebyte.io>
2021-04-28 22:52:32 +0800

    add/update/del user同时操作krnt_db.tw_admin

--- src/rbac/user.go
+++ src/rbac/user.go
@@ -2,6 +2,11 @@ package rbac
 
 import (
 	m "fjjozjn/admin/src/models"
+	"github.com/astaxie/beego/orm"
+	"crypto/md5"
+	"strings"
+	"time"
+	"fmt"
 )
 
 type UserController struct {
@@ -46,6 +51,22 @@ func (this *UserController) AddUser() {
 	}
 	id, err := m.AddUser(&u)
 	if err == nil && id > 0 {
+
+		//新增用户时同时插入用户数据到krnt_db
+		myDb := orm.NewOrm()
+		sNow := time.Now().Format("2006-01-02 15:04:05")
+		bPassword := []byte("jdf932n" + u.Password)
+		bEmailData := strings.Split(u.Email, "@")
+		_, err := myDb.Raw("insert into krnt_db.tw_admin set AdminID = ?, AdminLogin = ?, AdminPassword = ?, AdminName = ?, AdminNameChi = ?, " +
+			"AdminEmail = ?, AdminEmailRealName = ?, AdminEnabled = ?, AdminGrpID = ?, AdminCreateDate = ?, AdminPerm = ?, " +
+			"AdminLuxGroup = ?, AdminJoinDate = ?, AdminPlatform = ?, mode = ?", id, u.Username, fmt.Sprintf("%x", md5.Sum(bPassword)),
+			u.Nickname, u.Nickname, u.Email, bEmailData[0],
+			1, 1, sNow, -1, "admin", sNow, "sys|fty", 1).Exec()
+		if err != nil {
+			this.Rsp(false, "Create tw_admin user false - " + err.Error())
+			return
+		}
+
 		this.Rsp(true, "Success")
 		return
 	} else {
@@ -64,6 +85,41 @@ func (this *UserController) UpdateUser() {
 	}
 	id, err := m.UpdateUser(&u)
 	if err == nil && id > 0 {
+
+		//修改用户时同时修改krnt_db用户数据
+		myDb := orm.NewOrm()
+		if len(u.Nickname) > 0 {
+			_, err := myDb.Raw("update krnt_db.tw_admin set AdminName = ?, AdminNameChi = ? " +
+				"where AdminID = ?", u.Nickname, u.Nickname, u.Id).Exec()
+			if err != nil {
+				this.Rsp(false, "Update tw_admin user (Nickname) false - " + err.Error())
+				return
+			}
+		}
+		if len(u.Email) > 0 {
+			bEmailData := strings.Split(u.Email, "@")
+			_, err := myDb.Raw("update krnt_db.tw_admin set AdminEmail = ?, " +
+				"AdminEmailRealName = ? where AdminID = ?", u.Email, bEmailData[0], u.Id).Exec()
+			if err != nil {
+				this.Rsp(false, "Update tw_admin user (Email) false - " + err.Error())
+				return
+			}
+		}
+		if u.Status != 0 {
+			iAdminEnabled := 0
+			if u.Status == 1 {
+				iAdminEnabled = 2
+			} else if u.Status == 2 {
+				iAdminEnabled = 1
+			}
+			_, err := myDb.Raw("update krnt_db.tw_admin set AdminEnabled = ? " +
+				"where AdminID = ?", iAdminEnabled, u.Id).Exec()
+			if err != nil {
+				this.Rsp(false, "Update tw_admin user (Status) false - " + err.Error())
+				return
+			}
+		}
+
 		this.Rsp(true, "Success")
 		return
 	} else {
@@ -77,6 +133,15 @@ func (this *UserController) DelUser() {
 	Id, _ := this.GetInt64("Id")
 	status, err := m.DelUserById(Id)
 	if err == nil && status > 0 {
+
+		//删除用户时同时删除krnt_db用户数据
+		myDb := orm.NewOrm()
+		_, err := myDb.Raw("delete from krnt_db.tw_admin where AdminID = ?", Id).Exec()
+		if err != nil {
+			this.Rsp(false, "Delete tw_admin user false - " + err.Error())
+			return
+		}
+
 		this.Rsp(true, "Success")
 		return
 	} else {
